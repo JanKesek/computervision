5da119aa4121378f55e6153a39be0d1c0eab3a93
JanKesek <4chewbaca@gmail.com>
2020-08-26 16:25:23 +0200

    Working on all_screens bug

--- bossaexchange.py
+++ bossaexchange.py
@@ -67,8 +67,8 @@ if not os.path.isdir("exchange"):
 for l in os.listdir("exchange"):
         os.remove("exchange/{}".format(l))       
 while True:
-        #img=ImageGrab.grab()
-        img=Image.open("infoscreen.png")
+        img=ImageGrab.grab(all_screens=True)
+        #img=Image.open("infoscreen.png")
         width,height=img.size
         img=img.crop((0,40,width/2, height/10))
         #img=invertImage(img)
