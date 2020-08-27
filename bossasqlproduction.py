c1deb116a9b9977811276ce162fb0087cc63141d
JanKesek <4chewbaca@gmail.com>
2020-08-27 17:32:41 +0200

    manually setting coordinates

--- bossasqlproduction.py
+++ bossasqlproduction.py
@@ -3,14 +3,15 @@ from time import sleep, time
 from PIL import ImageGrab, Image, ImageOps
 import sys
 import os
-from plyer import notification
-from twilio.rest import Client
 import sqlite3
 import datetime
 import re
 import tkinter as tk
-import requests
+#import requests
 import datetime
+from mousepos import CropApp
+from notifications import send_sms,set_twilio_key,input_twilio_key,send_push, send_os_notification
+
 class SQLiteConn:
         @staticmethod
         def connect():
@@ -70,6 +71,9 @@ def invertImage(image):
                 final_image = ImageOps.invert(image)
         return final_image
 def write_file(file_path,info,file_index,after_parse=0):
+        if type(info)!=str:
+                info.save(file_path.format(file_index))
+                return
         if after_parse==1: file_path=file_path.format(file_index)
         else: file_path=file_path.format(file_index)+'bytes'
         with open(file_path,'w') as f:
@@ -84,12 +88,6 @@ def read_file(file_path,file_index,after_parse=1):
         with open(file_path,'r') as f:
                 s=f.read()
         return s
-def send_sms(info):
-        global twilio_keys
-        account_sid,auth_token,phonenumber=twilio_keys
-        client=Client(account_sid,auth_token)
-        message="New BOSSA news! {}".format(info)
-        client.messages.create(from_='+12053868878',to=('+48'+phonenumber),body=message)
 def alert_new_message(file_path,info,file_index):
         prev_hour=find_prev_newest_msg(file_index)
         if prev_hour!=None: prev_hour=prev_hour[0]
@@ -99,14 +97,12 @@ def alert_new_message(file_path,info,file_index):
         if hour!=prev_hour:
                 if is_bossa_message(info):
                         #notification.notify(title="New BOSSA message!",message=message)
+                        send_os_notification(hour,message)
                         SQLiteConn.insert_message(hour,message)
-                        send_push(title=hour,message=message)      
+                        print("New message found sending message")
+                        #send_push(title=hour,message=message)      
                         #send_sms(info)
                         #write_file(file_path,info,file_index,after_parse=1)
-def send_push(title,message):
-        payload={'title':title,'body': message,"date": datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}
-        requests.post("https://34.68.138.17/send",json=payload,verify=False)
-        #requests.post("https://localhost:3000/send",json=payload, verify=False)
 def is_bossa_message(message):
         hour=message.split(" ")[0]
         print("CHECKING IF HOUR MATCHES REGEX:{}".format(hour))
@@ -119,35 +115,25 @@ def parse_screen_data(datain, file_index):
         #datain="\n\n".join(datain[3:7])
         datain="".join(datain[0])
         return datain
-def set_twilio_key(ssid,auth,phonenumber,master=None):
-        global twilio_keys
-        twilio_keys=[ssid,auth,phonenumber]
-        with open("api.txt",'w') as f:
-                f.write(' '.join([ssid,auth,phonenumber]))
-        if master!=None: master.destroy()
-def input_twilio_key():
-        if os.path.exists("api.txt"):
-                with open("api.txt",'r') as f:
-                        keys=f.read().strip().split(" ")
-                        set_twilio_key(keys[0],keys[1],keys[2])
-        else:
-                master = tk.Tk()
-                tk.Label(master, text="Provide API keys to Twilio for SMS https://www.twilio.com").grid(row=0)
-                tk.Label(master, text="Account SSID key: ").grid(row=1)
-                tk.Label(master, text="Account auth key: ").grid(row=2)
-                tk.Label(master, text="Your phone number: ").grid(row=3)
-                e1 = tk.Entry(master)
-                e1.grid(row=1, column=1)
-                e2 = tk.Entry(master)
-                e2.grid(row=2, column=1)
-                e3 = tk.Entry(master)
-                e3.grid(row=3, column=1)
-                button=tk.Button(master, text='Confirm', 
-                        command=lambda e1=e1,e2=e2,e3=e3,m=master: set_twilio_key(e1.get(), e2.get(),e3.get(),m))
-                button.grid(row=3, column=0, sticky=tk.W, pady=4)
-                tk.mainloop()
+
+#FUNCTION RETURNS: FULL IMAGE, CROPPED IMAGE, IMAGE COORDINATES
+def crop_screenshot(filepath,xy,imgFull):
+        if len(xy)==0:
+                print("BEFORE CROP",imgFull)
+                crop=CropApp(imgFull)
+                print("AFTER CROP")
+                xy=crop.xy
+                print(xy)
+        print("OUR COORDINATES AFTER CROP: ",xy)
+        #img=imgFull.crop((38,3,1012,505))
+        img=imgFull.crop((xy['x1'],xy['y1'],xy['x2'],xy['y2']))
+        return img,xy
+        #img=Image.open("infoscreen.png")
+        #img=imgFull.crop((0,40,width/2, height/10))
+
 if __name__ == "__main__":
         #input_twilio_key()
+        #print("Prosze ustawic ekran na program BOSSA Nol")
         log_file=open("logs.txt","w")
         sys.stdout=log_file
         SQLiteConn.connect()
@@ -157,18 +143,28 @@ if __name__ == "__main__":
         if not os.path.isdir("exchange"):
                 os.makedirs("exchange")
         #for l in os.listdir("exchange"):
-        #        os.remove("exchange/{}".format(l))       
+        #        os.remove("exchange/{}".format(l))
+        #sleep(3)
+        xycrop={'x1':0,'y1':0,'x2':100,'y2':100}
         while True:
-                img=ImageGrab.grab(all_screens=True)
-                #img=Image.open("infoscreen.png")
-                width,height=img.size
-                img=img.crop((0,40,width/2, height/10))
+                #FUNCTION RETURNS: FULL IMAGE, CROPPED IMAGE, IMAGE COORDINATES
+                imgFull=ImageGrab.grab(all_screens=True)
+                full_img_path="exchange/fullImage{}.png"
+                cropped_img_path="exchange/croppedImage{}.png"
+                write_file(full_img_path,imgFull,i)
+                #img,xycrop=crop_screenshot(full_img_path.format(i),xycrop,imgFull) 
+                img=imgFull.crop((xycrop['x1'],xycrop['y1'],xycrop['x2'],xycrop['y2']))                      
+                #print(xycrop, img)
+                write_file(cropped_img_path,img,i)
+                if i!=0:
+                        os.remove(full_img_path.format(i))
+                        os.remove(cropped_img_path.format(i))
                 info=pytesseract.image_to_string(img)
                 file_path='exchange/output{}.txt'
                 write_file(file_path,info,i)
                 s=read_file(file_path,i, after_parse=0)
-                infor = parse_screen_data(s,i)
-                alert_new_message(file_path,infor,i)
+                infoParsed= parse_screen_data(s,i)
+                alert_new_message(file_path,infoParsed,i)
                 os.remove(file_path.format(i)+'bytes')       
                 i+=1
                 sleep(2)
