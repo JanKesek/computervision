c7e4626951bb1fa40491e19e32fd2578d25fd1c3
JanKesek <4chewbaca@gmail.com>
2020-09-10 17:58:38 +0200

    Testing pyrobot

--- choosemonitor.py
+++ choosemonitor.py
@@ -2,6 +2,6 @@ from pyrobot import Robot
 robot=Robot()
 monitors=robot.get_display_monitors()
 print(monitors)
-
+print(len(monitors))
 im=robot.take_screenshot(monitors[-1])
 im.save('screenshotwhatever.png','png')
